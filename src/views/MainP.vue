456c72485a4fec91270bf6a4c6523b5b906efb64
guchen <1023797278@qq.com>
2022-07-08 23:11:26 +0800

    a new function

--- src/views/MainP.vue
+++ src/views/MainP.vue
@@ -18,15 +18,37 @@
     </div>
     <div
       class="r"
-      :style="{ 'min-height': height + 'px' }"
+      :style="{ 'min-height': height + 0.5 + 'px' }"
       style="background-color: blue"
     >
       1
     </div>
+    <div style="height: 60px">1</div>
   </el-scrollbar>
 </template>
 
 <script>
+var scrollFunction = function (e) {
+  e = e || window.event;
+  e.preventDefault && e.preventDefault(); //禁止浏览器默认事件
+  if (e.wheelDelta) {
+    //判断浏览器IE，谷歌滑轮事件
+    if (e.wheelDelta > 0) {
+      //当滑轮向上滚动时
+    }
+    if (e.wheelDelta < 0) {
+      //当滑轮向下滚动时
+    }
+  } else if (e.detail) {
+    //Firefox滑轮事件
+    if (e.detail > 0) {
+      //当滑轮向上滚动时
+    }
+    if (e.detail < 0) {
+      //当滑轮向下滚动时
+    }
+  }
+};
 export default {
   data() {
     return {
@@ -34,6 +56,8 @@ export default {
       page: 0,
       oldcount: 0,
       lock: 0,
+      maxpage: 2,
+      show: 0,
     };
   },
   methods: {
@@ -42,18 +66,27 @@ export default {
       this.height = document.body.clientHeight - 60;
     },
     count() {
-      if (this.lock != 1)
-        if (this.$refs.scs.wrap$.scrollTop >= this.oldcount)
+      if (this.lock != 1) {
+        if (this.$refs.scs.wrap$.scrollTop - this.maxpage * this.height > 10) {
+          this.page = this.maxpage + 1;
+          return;
+        }
+        if (this.$refs.scs.wrap$.scrollTop >= this.oldcount) {
+          this.show = 1;
           this.page = parseInt(
             (this.$refs.scs.wrap$.scrollTop + this.height / 1.5) / this.height
           );
-        else
+        } else {
+          this.show = 0;
           this.page = parseInt(
             (this.$refs.scs.wrap$.scrollTop + this.height / 3) / this.height
           );
+        }
+      }
+
       this.oldcount = this.$refs.scs.wrap$.scrollTop;
     },
-    ScrollTop(number = 0, time) {
+    ScrollTop(number = 0, time, fix = 0) {
       if (!time) {
         this.$refs.scs.wrap$.scrollTop = number;
         return number;
@@ -68,7 +101,13 @@ export default {
           this.ScrollTop((nowTop += everTop));
         } else {
           clearInterval(scrollTimer); // 清除计时器
-          this.lock = 0;
+          if (fix == 0) {
+            this.timeout = setTimeout(() => {
+              this.ScrollTop(number, 100, 1);
+            }, 100);
+          } else {
+            this.lock = 0;
+          }
         }
       }, spacingTime);
     },
@@ -84,10 +123,30 @@ export default {
     page() {
       //this.$refs.scs.scrollTo(0, 729 * this.page);
       this.lock = 1;
-      this.ScrollTop(729 * this.page, 200);
-
-      console.log(this.lock);
+      if (this.page == this.maxpage) this.ScrollTop(this.height * this.page + 0.5, 200);
+      else this.ScrollTop(this.height * this.page, 200);
       //console.log(this.page);
+    },
+    lock() {
+      if (this.lock == 1) {
+        if (document.addEventListener) {
+          //firefox
+          document.addEventListener("DOMMouseScroll", scrollFunction, false);
+        }
+        //滚动滑轮触发scrollFunction方法  //ie 谷歌
+        window.addEventListener("mousewheel", scrollFunction, {
+          passive: false,
+        });
+      } else {
+        if (document.removeEventListener) {
+          //firefox
+          document.removeEventListener("DOMMouseScroll", scrollFunction, false);
+        }
+        //滚动滑轮触发scrollFunction方法  //ie 谷歌
+        window.removeEventListener("mousewheel", scrollFunction, {
+          passive: false,
+        });
+      }
     },
   },
 };
